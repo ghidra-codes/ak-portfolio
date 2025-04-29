79dbfb9179edec985ddf7724ca80a33db916c439
Alexander Kallin <akallin.94@gmail.com>
2025-04-29 19:44:28 +0200

    added icons for tools

---
+++ src/components/Tools.tsx
@@ -0,0 +1,12 @@
+import git from "../assets/images/icons/git.svg";
+
+const Tools = () => {
+	return (
+		<div className="tools">
+			<h2>Frontend technologies I use</h2>
+			<img src={git} alt="Git icon" />
+		</div>
+	);
+};
+
+export default Tools;
