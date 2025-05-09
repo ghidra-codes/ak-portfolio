6ad7c752356f89938719a803f4e816234cfedae1
Alexander Kallin <akallin.94@gmail.com>
2025-05-09 12:05:29 +0200

    created conditional rendering for touch devices; created component for checking screen width

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,9 +1,11 @@
-import AnimateOnScroll from "./AnimateOnScroll";
 import React, { useState } from "react";
 import { techStack, categoryInfo } from "../data/techStack";
 import CursorInfoBox from "./CursorInfoBox";
 import useIsTouchDevice from "../hooks/useIsTouchDevice";
 import TechStackIcon from "./TechStackIcon";
+import TechStackIconTouch from "./TechStackIconTouch";
+
+type Tech = (typeof techStack)[number];
 
 const TechStack = () => {
 	const [hoveredCategory, setHoverCategory] = useState<string | null>(null);
@@ -16,33 +18,65 @@ const TechStack = () => {
 	const handleMouseMove = (e: React.MouseEvent) => setCursorPos({ x: e.clientX, y: e.clientY });
 	const handleMouseEnter = (info: string) => setHoveredInfo(info);
 
+	const groupedByCategory = techStack.reduce((acc, tech) => {
+		if (!acc[tech.category]) acc[tech.category] = [];
+		acc[tech.category].push(tech);
+		return acc;
+	}, {} as Record<string, Tech[]>);
+
 	return (
 		<>
 			<h2 className="section-header" id="tech-section-heading">
 				Technologies I’ve worked with
 			</h2>
-			<AnimateOnScroll className="tech-stack">
-				<div className="icon-wrapper" onMouseMove={handleMouseMove}>
-					{techStack.map(({ name, icon, category }) => {
-						const isHighlighted = hoveredCategory === null || hoveredCategory === category;
-						const isLabelHighlighted = hoveredCategory === category;
+			<div className="tech-stack">
+				<div className={isTouchDevice ? "icon-wrapper-touch" : "icon-wrapper"} onMouseMove={handleMouseMove}>
+					{!isTouchDevice
+						? techStack.map(({ name, icon, category }) => {
+								const isHighlighted = hoveredCategory === null || hoveredCategory === category;
+								const isLabelHighlighted = hoveredCategory === category;
 
-						return (
-							<TechStackIcon
-								key={name}
-								name={name}
-								icon={icon}
-								category={category}
-								isHighlighted={isHighlighted}
-								isLabelHighlighted={isLabelHighlighted}
-								isTouchDevice={isTouchDevice}
-								onHoverChange={(newCategory) => {
-									setHoverCategory(newCategory);
-									handleMouseEnter(newCategory ?? "");
-								}}
-							/>
-						);
-					})}
+								return (
+									<TechStackIcon
+										key={name}
+										name={name}
+										icon={icon}
+										category={category}
+										isHighlighted={isHighlighted}
+										isLabelHighlighted={isLabelHighlighted}
+										isTouchDevice={isTouchDevice}
+										onHoverChange={(newCategory) => {
+											setHoverCategory(newCategory);
+											handleMouseEnter(newCategory ?? "");
+										}}
+									/>
+								);
+						  })
+						: Object.entries(groupedByCategory).map(([category, items]) => (
+								<div key={category} className="tech-category-group">
+									<div className="category-heading-wrapper">
+										<img
+											className="category-heading-icon"
+											src={categoryInfo[category].icon}
+											alt={categoryInfo[category].label}
+										/>
+										<h3 className="category-heading">{categoryInfo[category].label}</h3>
+									</div>
+									<div className="icon-wrapper">
+										{items.map(({ name, icon }, index) => {
+											const isOdd = items.length % 2 === 1 && index === items.length - 1;
+
+											return !isOdd ? (
+												<TechStackIconTouch key={name} name={name} icon={icon} />
+											) : (
+												<div className="centered-icon-wrapper">
+													<TechStackIconTouch key={name} name={name} icon={icon} />
+												</div>
+											);
+										})}
+									</div>
+								</div>
+						  ))}
 				</div>
 				{hoveredInfo && !isTouchDevice && (
 					<CursorInfoBox
@@ -52,7 +86,7 @@ const TechStack = () => {
 						label={categoryInfo[hoveredInfo].label}
 					/>
 				)}
-			</AnimateOnScroll>
+			</div>
 		</>
 	);
 };
---
+++ src/components/TechStackIconTouch.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type TechStackIconTouchProps = {
+	icon: string;
+	name: string;
+};
+
+const TechStackIconTouch: React.FC<TechStackIconTouchProps> = ({ icon, name }) => {
+	return (
+		<div className="icon-container">
+			<img src={icon} alt={`${name} icon`} />
+			<p className="icon-label">{name}</p>
+		</div>
+	);
+};
+
+export default TechStackIconTouch;
