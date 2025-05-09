83fb13c4cdac13d8b5084325e8e7b764fb327cb6
Alexander Kallin <alexander@levnadsglad.se>
2025-05-10 00:21:37 +0200

    added a slight background gradient for category headings

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
+import useMediaQuery from "../hooks/useMediaQuery";
 
 export default function NavBar() {
+	const isSmallScreen = useMediaQuery("(max-width: 800px)");
 	return (
 		<nav className="navbar">
 			<div className="brand">
 				<div className="logo-wrapper"></div>
-				<h1 className="navbar-heading">Alexander Kallin</h1>
+				{!isSmallScreen && <h1 className="navbar-heading">Alexander Kallin</h1>}
 			</div>
 			<ul className="navbar-links">
 				<li className="home-link">
