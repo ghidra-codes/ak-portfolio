2b9cd16bfc5df58f3f396be0138761025e4380db
Alexander Kallin <akallin.94@gmail.com>
2025-05-13 15:19:47 +0200

    worked on styling for the dropdown navbar menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import useMediaQuery from "../hooks/useMediaQuery";
-import HamburgerMenu from "./HamburgerMenu";
+import HamburgerMenuBtn from "./HamburgerMenuBtn";
 import NavBarLinks from "./NavBarLinks";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function NavBar() {
 	const isSmallScreen = useMediaQuery("(max-width: 920px)");
 
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const onToggle = () => setIsMenuOpen((prev) => !prev);
+
 	return (
-		<nav className="navbar">
-			<div className="brand">
-				<div className="logo-wrapper"></div>
-				{!isSmallScreen && <h1 className="navbar-heading">Alexander Kallin</h1>}
-			</div>
-			{isSmallScreen ? <HamburgerMenu /> : <NavBarLinks />}
-		</nav>
+		<>
+			<nav className="navbar">
+				<div className="brand">
+					<div className="logo-wrapper"></div>
+					{!isSmallScreen && <h1 className="navbar-heading">Alexander Kallin</h1>}
+				</div>
+				{isSmallScreen ? <HamburgerMenuBtn onToggle={onToggle} active={isMenuOpen} /> : <NavBarLinks />}
+			</nav>
+
+			<AnimatePresence>
+				{isMenuOpen && isSmallScreen && (
+					<motion.div
+						key="menu"
+						initial={{ opacity: 0, x: "100%" }}
+						animate={{ opacity: 1, x: 0 }}
+						exit={{ opacity: 0, x: "100%" }}
+						transition={{ duration: 0.3, ease: "easeInOut" }}
+						className="menu-wrapper"
+					>
+						<NavBarLinks variant="hamburger" onLinkClick={onToggle} />
+					</motion.div>
+				)}
+			</AnimatePresence>
+		</>
 	);
 }
