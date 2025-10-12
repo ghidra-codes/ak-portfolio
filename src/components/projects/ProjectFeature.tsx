cb8d4883dd619a32bbe16104beef6d9d8f07230a
Alexander Kallin <alexander@levnadsglad.se>
2025-10-12 20:12:44 +0200

    worked on responsiveness for project feature and created setup for mobile layout

--- src/components/projects/ProjectFeature.tsx
+++ src/components/projects/ProjectFeature.tsx
@@ -2,6 +2,7 @@ import TintedImage, { TintedImageProps } from "../ui/TintedImage";
 import React from "react";
 import { FiGithub } from "react-icons/fi";
 import { FiLink } from "react-icons/fi";
+import { useMediaQuery } from "react-responsive";
 
 type ProjectImageProps = Omit<TintedImageProps, "children" | "triggerPopIn">;
 interface ProjectFeatureProps {
@@ -17,22 +18,43 @@ const ProjectFeature: React.FC<ProjectFeatureProps> = ({
 	description,
 	projectIcons,
 }) => {
+	const isSmallScreen = useMediaQuery({ maxWidth: 1024 });
+
 	return (
-		<div className="project-feature">
-			<div className="left">
-				<TintedImage {...imageProps} />
-				<div className="project-icons">{projectIcons}</div>
-			</div>
-			<div className="right">
-				<div className="project-heading">
-					<span>
-						<FiGithub className="project-link-icon" />
-						<FiLink className="project-link-icon" />
-					</span>
-					{heading}
-				</div>
-				<div className="project-description">{description}</div>
-			</div>
+		<div className={`project-feature ${isSmallScreen ? "is-mobile" : "is-desktop"}`}>
+			{isSmallScreen ? (
+				<>
+					<div className="top">
+						<TintedImage {...imageProps} />
+						{heading}
+					</div>
+					<div className="bottom">
+						<div className="project-icons">{projectIcons}</div>
+						<span>
+							<FiGithub className="project-link-icon" />
+							<FiLink className="project-link-icon" />
+						</span>
+						<div className="project-description">{description}</div>
+					</div>
+				</>
+			) : (
+				<>
+					<div className="left">
+						<TintedImage {...imageProps} />
+						<div className="project-icons">{projectIcons}</div>
+					</div>
+					<div className="right">
+						<div className="project-heading">
+							<span>
+								<FiGithub className="project-link-icon" />
+								<FiLink className="project-link-icon" />
+							</span>
+							{heading}
+						</div>
+						<div className="project-description">{description}</div>
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
